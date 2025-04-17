ba049b688b2faf55722066c6acd0cb147f61fcd4
Tuan Duong <duongquoctuan8999@gmail.com>
2025-04-17 23:08:30 +0700

    Auto-generated commit

---
+++ chunk-FCC3SJTL.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var t=[{path:"",loadComponent:()=>import("./chunk-BKA747NP.js").then(o=>o.SimpleImageEditorComponent)}];export{t as IMAGE_EDITOR_ROUTES};
--- chunk-NP6EL7QT.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var t=[{path:"",loadComponent:()=>import("./chunk-62HO3MLF.js").then(o=>o.SimpleImageEditorComponent)}];export{t as IMAGE_EDITOR_ROUTES};
